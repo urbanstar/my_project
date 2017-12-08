2b0a88ed775c4c5a0e71b4742039204a9bde27df
Unknown <urstoo@mail.ru>
2017-12-08 20:09:44 +0300

    Auto stash before merge of "master" and "origin/master"
    добавлены мшуц в проект

---
+++ view/v_log_pas.php
@@ -0,0 +1,36 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>add article</title>
+    <!--                                                              Bootstrap core CSS
+   ====================================================================================================================================================== -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ======================================================================================================================================================
+                                                                      End Bootstrap core CSS.-->
+    <!--                                                              Custom styles for this template
+   ====================================================================================================================================================== -->
+    <link href="../assets/css/justified-nav.css" rel="stylesheet">
+    <!-- ======================================================================================================================================================
+                                                                      End Custom styles for this template.-->
+</head>
+<header class="masthead">
+    <h3 class="text-muted"><?=$title?></h3>
+</header>
+<form action="../controller/c_log_pas.php"  method="post">
+    <div class="form-group">
+        <b><label for="exampleInputLogin1">Введите логин</label></b>
+        <input type="input" class="form-control" id="exampleInputlog" name ='log' aria-describedby="emailHelp" placeholder="Введите логин">
+        <small id="emailHelp" class="form-text text-muted">Обязательное поле</small>
+    </div>
+    <div class="form-group">
+        <b> <label for="exampleInputPass1">Введите пароль</label></b>
+        <input type="password" class="form-control" id="exampleInputPass1" name='pass' placeholder="Пароль">
+        <small id="emailHelp" class="form-text text-muted">Обязательное поле</small>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Вход</button>
+</form>
---
+++ view/v_read.php
@@ -0,0 +1,8 @@
+   <div class="card">
+           <h4 class="card-header"><?=$title?></h4>
+       <div class="card-body">
+
+           <p class="card-text"><?=$text_article?></p>
+           <a  class="btn btn-primary" href="index.php">Вернуться на главную</a>
+       </div>
+   </div>
